259a54370098a7cc45d3d8275df1ca71662fcaf4
Jaehyun Yeom <jae.yeom@gmail.com>
2014-02-09 19:20:52 -0800

    Remove unnecessary fakeReader from tests. bytes.Reader already implements this.

--- go/sstable/entry_test.go
+++ go/sstable/entry_test.go
@@ -1,8 +1,8 @@
 package sstable
 
 import (
+	"bytes"
 	"fmt"
-	"io"
 )
 
 func ExampleEntryMarshalBinary() {
@@ -30,41 +30,17 @@ func ExampleEntryUnmarshalBinary() {
 	// {[1 2 3] [5 6 7 8]}
 }
 
-type fakeReader struct {
-	buf    []byte
-	offset int
-}
-
-func (f *fakeReader) Read(p []byte) (n int, err error) {
-	if f.offset >= len(f.buf) {
-		return 0, io.EOF
-	}
-	n = copy(p, f.buf[f.offset:])
-	f.offset += n
-	return n, nil
-}
-
-func (f *fakeReader) ReadAt(b []byte, offset int64) (n int, err error) {
-	if int64(len(f.buf)) < offset {
-		return 0, io.EOF
-	}
-	if int64(len(f.buf)) <= offset+int64(len(b)) {
-		return copy(b, f.buf[offset:]), io.EOF
-	}
-	return copy(b, f.buf[offset:]), nil
-}
-
 func ExampleReadEntry() {
-	f := fakeReader{buf: []byte{0, 0, 0, 3, 0, 0, 0, 4, 1, 2, 3, 5, 6, 7, 8}}
-	e, _ := readEntry(&f)
+	f := bytes.NewReader([]byte{0, 0, 0, 3, 0, 0, 0, 4, 1, 2, 3, 5, 6, 7, 8})
+	e, _ := readEntry(f)
 	fmt.Println(e)
 	// Output:
 	// &{[1 2 3] [5 6 7 8]}
 }
 
 func ExampleReadEntryAt() {
-	f := fakeReader{buf: []byte{0, 0, 0, 3, 0, 0, 0, 4, 1, 2, 3, 5, 6, 7, 8}}
-	e, _ := readEntryAt(&f, 0)
+	f := bytes.NewReader([]byte{0, 0, 0, 3, 0, 0, 0, 4, 1, 2, 3, 5, 6, 7, 8})
+	e, _ := readEntryAt(f, 0)
 	fmt.Println(e)
 	// Output:
 	// &{[1 2 3] [5 6 7 8]}
